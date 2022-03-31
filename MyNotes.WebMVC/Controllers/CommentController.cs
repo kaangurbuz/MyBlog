acda1ebecada240564c6ffe5dd0911791acb05d4
Kaan Gürbüz <kaangurbuz97@gmail.com>
2022-03-31 14:45:53 +0300

    login, comment popup

---
+++ MyNotes.WebMVC/Controllers/CommentController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyNotes.BusinessLayer;
+using MyNotes.DataAccessLayer;
+using MyNotes.EntityLayer;
+
+namespace MyNotes.WebMVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private MyNoteContext db = new MyNoteContext();
+        private NoteManager nm = new NoteManager();
+        private CommentManager cm = new CommentManager();
+
+        // GET: Comment
+        public ActionResult Index()
+        {
+            return View(db.Comments.ToList());
+        }
+
+        // GET: Comment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comment/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Text,CreatedOn,ModifiedOn,ModifiedUserName")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(comment);
+        }
+
+        // GET: Comment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Text,CreatedOn,ModifiedOn,ModifiedUserName")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comment);
+        }
+
+        // GET: Comment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ShowNoteComments(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var note = nm.QList().Include("Comments").FirstOrDefault(s => s.Id == id);
+            if (note == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            return PartialView("_PartialComments", note.Comments);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- MyNotes.WebMVC/Controllers/HomeController.cs
+++ MyNotes.WebMVC/Controllers/HomeController.cs
@@ -4,11 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyNotes.BusinessLayer;
+using MyNotes.BusinessLayer.ValueObject;
+using MyNotes.EntityLayer;
+using MyNotes.EntityLayer.Messages;
 
 namespace MyNotes.WebMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MyNotesUserManager mum = new MyNotesUserManager();
+        private BusinessLayerResult<MyNotesUser> res;
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                res = mum.LoginUser(model);
+                if (res.Errors.Count>0)
+                {
+                    if ((res.Errors.Find(x=>x.Code==ErrorMessageCode.UserIsNotActive)!=null))
+                    {
+                        ViewBag.SetLink = "http://Home/UserActivate/1234-2345-3456789";
+                    }
+                    res.Errors.ForEach(x=>ModelState.AddModelError("",x.Message));
+                    return View(model);
+                }
+
+                Session["Login"] = res.Result;
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
         public ActionResult Index()
         {
             Test test = new Test();
