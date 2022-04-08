563b9674d3156ad6be2d3973b1b15f5f66f482ed
Kaan Gürbüz <kaangurbuz97@gmail.com>
2022-04-08 14:42:13 +0300

    kategori işlemleri, cache,

--- MyNotes.WebMVC/Controllers/CategoryController.cs
+++ MyNotes.WebMVC/Controllers/CategoryController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MyNotes.BusinessLayer;
+using MyNotes.BusinessLayer.Models;
+using MyNotes.BusinessLayer.ValueObject;
 
 namespace MyNotes.WebMVC.Controllers
 {
@@ -13,7 +16,61 @@ namespace MyNotes.WebMVC.Controllers
         // GET: Category
         public ActionResult Index()
         {
-            return View(cm.List());
+            var cat = cm.IndexCat();
+            return View(cat);
+            //return View(cm.List());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            CategoryViewModel cvm = cm.FindCat(id);
+            if (cvm == null)
+            {
+                return HttpNotFound();
+            }
+            //CategoryViewModel cvm = new CategoryViewModel();
+            //cvm.Id = cat.Id;
+            //cvm.Title = cat.Title;
+            //cvm.Description = cat.Description;
+            //cvm.CreatedOn = cat.CreatedOn;
+            //cat.ModifiedOn = cat.ModifiedOn;
+            //cat.ModifiedUserName = cat.ModifiedUserName;
+            return View(cvm);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CategoryViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                cm.InsertCat(cvm);
+                CacheHelper.RemoveCategoriesCache();
+                return RedirectToAction("Index");
+            }
+            return View(cvm);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CategoryViewModel cvm = cm.FindCat(id);
+            if (cvm == null)
+            {
+                return HttpNotFound();
+            }
+            cm.DeleteCat(id);
+            return RedirectToAction("Index");
         }
     }
 }
